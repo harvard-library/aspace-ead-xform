20ba888761951358b086be60c1d05eb5b55f0f65
Hurlyburly2 <douglas_simon@harvard.edu>
2020-02-07 09:59:04 -0500

    resolved download csv not appearing until refresh issue

--- frontend/assets/ead_xform.js
+++ frontend/assets/ead_xform.js
@@ -1,28 +1,10 @@
-/* any js needed for EAD transforms */
-$(function () {
-    var csvBtnAdded = (typeof  ead_xform_id === 'undefined'); /* if we don't have the id to beginwith, forget it*/    
-/* the defer methodolgy is adapted from https://stackoverflow.com/questions/7486309/how-to-make-script-execution-wait-until-jquery-is-loaded */
-/* since the lower half of the screen loads *after* the javascript is loaded, we need to to this */
-    function defer(method) {
-	if ($("#download-ead-dropdown").length > 0) {
-	    method();
-	}
-	else {
-	    setTimeout(function() { defer(method) }, 50);
-	}
-    }
+// This is embedded directly in resources/toolbar.html.erb because ArchivesSpace core,
+// after visiting a resource page via link (but not on refresh), loads that component
+// twice, overwriting out any additions to the menu
 
-var addCsvBtn = function () {
-	if (!csvBtnAdded) {
-	    var $eadBtn = $("#download-ead-dropdown");
-	    if ($eadBtn.length > 0) {
-		a = '/resources/' +  ead_xform_id + '/staff_csv';
-		$eadBtn.after('<li class="hvd-download-csv"><a href="' + a + '">Download CSV</a></li>');
-		csvBtnAdded = true;
-	    }
-	}
-    };
+let $eadBtn = $("#download-ead-dropdown")
 
-    defer(addCsvBtn);
-
-});
+if ($eadBtn) {
+	a = '/resources/' +  ead_xform_id + '/staff_csv';
+	$eadBtn.after('<li class="hvd-download-csv" id="hvd-download-csv"><a href="' + a + '">Download CSV</a></li>');
+}
